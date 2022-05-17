1e0c700adc2ca368276e5393218d7c808ba735cd
Nirmal2349 <nirmalrajr35@gmail.com>
2022-05-17 17:05:52 +0530

    addform

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
 import { useFormik } from "formik";
 import * as yup from "yup";
-import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 
 export default function App() {
   return (
     <div className="App">
-      <Loginform />
+      {/* <Loginform /> */}
+      {/* <Register/> */}
+      {/* <Addrestaurant /> */}
+      <Typetimings />
     </div>
   );
 }
@@ -85,4 +94,121 @@ function Loginform() {
     </div>
   );
 }
-// add Restaurant
+// New user
+function Register() {
+  return (
+    <div className="register-container">
+      <h3>Sign up</h3>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "30ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Full Name" variant="outlined" />
+        <TextField id="outlined-basic" label="Email" variant="outlined" />
+        <TextField
+          id="outlined-basic"
+          label="Mobile Number"
+          variant="outlined"
+        />
+        <TextField id="outlined-basic" label="Password" variant="outlined" />
+        <TextField
+          id="outlined-basic"
+          label="Confirm Password"
+          variant="outlined"
+        />
+        <Button
+          size="small"
+          variant="contained"
+          // onClick={() => history.goForward(`/rental`)}
+          aria-label="toggle descreption"
+        >
+          sign up
+        </Button>
+      </Box>
+    </div>
+  );
+}
+
+// add restaurant
+
+function Addrestaurant() {
+  return (
+    <div className="addrestaurant">
+      <h2>Restaurant details</h2>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "30ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="outlined-basic"
+          label="Restaurant Name"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-basic"
+          label="Restaurant Address "
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-basic"
+          label="Contact number at restaurant"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-basic"
+          label="Restaurant owner Name"
+          variant="outlined"
+        />
+        <Stack spacing={15} direction="row">
+          <Button variant="outlined">Back</Button>
+          <Button variant="contained">Next</Button>
+        </Stack>
+      </Box>
+    </div>
+  );
+}
+
+//restaurant type & timing
+
+function Typetimings() {
+  return (
+    <div className="typetiming">
+      <h2>Restaurant Type & Timings</h2>
+      <h5>Establishment type</h5>
+      <FormControl>
+        <FormLabel id="demo-radio-buttons-group-label">
+          Select most relevant category for your restaurant type
+        </FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="female"
+          name="radio-buttons-group"
+        >
+          <FormControlLabel
+            value="female"
+            control={<Radio />}
+            label="Both, delivery and dine-in available"
+          />
+          <FormControlLabel
+            value="male"
+            control={<Radio />}
+            label="Dine-in only"
+          />
+          <FormControlLabel
+            value="other"
+            control={<Radio />}
+            label="Delivery only"
+          />
+        </RadioGroup>
+      </FormControl>
+    </div>
+  );
+}
